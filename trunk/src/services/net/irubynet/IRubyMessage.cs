290e1affbae6da195c33b166dd499cc6e22e3212
nohros.ceo <nohros.ceo@15e307e2-2acb-11df-b744-258d51847b74>
2011-02-09 12:54:57 +0000

    added a interface that defines the methods and properties that all messages classes must implement.

---
+++ trunk/src/services/net/irubynet/IRubyMessage.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nohros.Ruby
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class IRubyMessage
+    {
+    }
+}
