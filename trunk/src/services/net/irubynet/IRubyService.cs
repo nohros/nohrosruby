ce17c3a411b31b4bc6184247aeab88e3a7abe2f1
nohros.ceo <nohros.ceo@15e307e2-2acb-11df-b744-258d51847b74>
2011-02-09 12:57:31 +0000

    added a new method to the service class that creates a way to send/receive messages from/to the service host.

--- trunk/src/services/net/irubynet/IRubyService.cs
+++ trunk/src/services/net/irubynet/IRubyService.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
 
 namespace Nohros.Ruby
 {
@@ -18,5 +19,40 @@ namespace Nohros.Ruby
         /// Starts the service.
         /// </summary>
         void Run();
+
+        /// <summary>
+        /// Handle messages sent from ruby server.
+        /// </summary>
+        /// <param name="message">The message sent from ruby server.</param>
+        /// <returns>true if the message was successfully processed; otherwise false.</returns>
+        /// <remarks>
+        /// The message dispatcher in the main thread of a ruby service process invokes the message handler
+        /// function for the specified service whenever it receives a message from the ruby server. After
+        /// receiving the message, the message handler function must return true if the message was
+        /// succesfully processed or false if not.
+        /// <para>
+        /// The message handler function is intended to receive messages and return immediately. The callback
+        /// function should save its parameters and create other threads to perform additional work. The ruby
+        /// service must ensure that such threads have exited before stopping the service. For this reason, it
+        /// is very recommended the use of background threads. In particular, a message handler should avoid
+        /// operations that might block, such taking a lock, because this could result in a deadlock or cause
+        /// the system to stop responding.
+        /// </para>
+        /// <para>
+        /// When the ruby service host sends a message to a service, it waits for the handler function to return
+        /// before sending additional messages. The message handler should return as quick as possible, if it does
+        /// not return within 30 seconds, the RSH returns an error. If a service must do lengthy processing when the
+        /// service is executing the message handler, it should create a secondary thread to perform the lengthy
+        /// processing, and then return from the message handler. This prevents the service from tying up the message
+        /// dispatcher.
+        /// </para>
+        /// </remarks>
+        bool OnServerMessage(IRubyMessage message);
+
+        /// <summary>
+        /// Gets the status information for a service. This property is used by services to report its current
+        /// status to the ruby service host.
+        /// </summary>
+        ServiceStatus Status { get; }
     }
 }
