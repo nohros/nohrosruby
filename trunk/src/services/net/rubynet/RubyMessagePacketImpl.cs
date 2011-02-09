2b7f71b94868dd1b8461911f4939e405d6243b6d
nohros.ceo <nohros.ceo@15e307e2-2acb-11df-b744-258d51847b74>
2011-02-09 12:44:50 +0000

    implements the IRubyMessagePacket for the class RubyMessagePacket that was generated by the protocol buffer compiler.

---
+++ trunk/src/services/net/rubynet/RubyMessagePacketImpl.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nohros.Ruby
+{
+    /// <summary>
+    /// Provides implementation of the interface <see cref="IRubyMessagePacket"/> for the class <see cref="RubyMessagePacket"/>
+    /// </summary>
+    /// <remarks>
+    /// All the ruby messages must implements the interface <see cref="IRubyMessagePacket"/>. The code of the
+    /// class <see cref="RubyMessagePacket"/> was partially generated from the protocol buffer compiler.
+    /// </remarks>
+    public sealed partial class RubyMessagePacket : IRubyMessagePacket
+    {
+    }
+}
