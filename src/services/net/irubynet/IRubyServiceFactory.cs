7f52678c160342150934f22b57ab60b7ba5f4afd
nohros.ceo <nohros.ceo@15e307e2-2acb-11df-b744-258d51847b74>
2012-04-09 16:06:36 +0000

    Defined a factory that is used to create instances of the IRubyService interface.

---
+++ src/services/net/irubynet/IRubyServiceFactory.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nohros.Ruby
+{
+  /// <summary>
+  /// Defines a way to create <see cref="IRubyService"/> objects inside the
+  /// library that implements the <see cref="IRubyService"/> interface.
+  /// </summary>
+  /// <remarks>
+  /// <para>
+  /// The <see cref="IRubyServiceFactory"/> interface implies a constructor
+  /// with the following signature:
+  /// <para>
+  /// <code>IRubyServiceFactory()</code>
+  /// </para>
+  /// </para>
+  /// This constructor is called by the ruby service host(RSH) in order to
+  /// create a new instance of the <see cref="IRubyService"/> interface.
+  /// </remarks>
+  public interface IRubyServiceFactory
+  {
+    /// <summary>
+    /// Creates a instance of the <see cref="IRubyService"/> interface.
+    /// </summary>
+    /// <param name="command_line_string">A string representing the command
+    /// line that was received by the ruby service host(RSH) as a parameter to
+    /// the service.</param>
+    /// <returns>An object the implements the <see cref="IRubyService"/>
+    /// interface.</returns>
+    IRubyService CreateService(string command_line_string);
+  }
+}
