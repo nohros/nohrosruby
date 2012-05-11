19a411bf4e0d23099d3bac841b5a2e53dd4830c1
nohros.ceo <nohros.ceo@15e307e2-2acb-11df-b744-258d51847b74>
2012-05-11 19:39:57 +0000

    Command line switches for the ruby service.

---
+++ trunk/src/node/service/ruby_switches.cc
@@ -0,0 +1,16 @@
+// Copyright (c) 2010 Nohros Systems Inc. All rights reserved
+// Use of this source code is governed by BSD-style license that can be found
+// in the LICENCE file.
+
+#include "node/service/ruby_switches.h"
+
+namespace switches {
+
+// ---------------------------------------------------------------------------
+// When commenting your switch, please use the same voice as surrounding
+// comments. Imagine "This switch..." at the beginning of the phrase, and it'll
+// all work out.
+// ---------------------------------------------------------------------------
+
+
+}  // namespace swicthes
---
+++ trunk/src/node/service/ruby_switches.h
@@ -0,0 +1,19 @@
+// Copyright (c) 2010 Nohros Systems Inc. All rights reserved
+// Use of this source code is governed by BSD-style license that can be found
+// in the LICENCE file.
+
+// Defines all the command-line switches used by Ruby.
+
+#ifndef NODE_SERVICE_RUBY_SWITCHES_H_
+#define NODE_SERVICE_RUBY_SWITCHES_H_
+#pragma once
+
+namespace switches {
+
+// All switches in alphabetical order. The switches should be documented
+// alongside the definition of their values in the .cc file.
+
+//extern const char k
+}
+
+#endif
