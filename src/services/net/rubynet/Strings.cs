1ccebe89354a407efe2ad3dff0dc1b265417ce60
nohros.ceo <nohros.ceo@15e307e2-2acb-11df-b744-258d51847b74>
2012-06-06 23:28:29 +0000

    Strings will hold all the strings constants used by the rubynet project.

---
+++ src/services/net/rubynet/Strings.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Nohros.Ruby.Service.Net
+{
+  internal sealed class Strings
+  {
+    public const string kLoggingProviderNode = "LoggingProvider";
+    public const string kMyToolsPackNode = "mytoolspack";
+  }
+}
