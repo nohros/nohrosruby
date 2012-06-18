c390bee1f406ea50c2181298d436b141c7b79f2b
nohros.ceo <nohros.ceo@15e307e2-2acb-11df-b744-258d51847b74>
2012-06-18 14:03:19 +0000

    Created a class to associate a IExecutor to a IRubyMessageListener.

---
+++ src/services/net/rubynet/ipc/ListenerExecutorPair.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Nohros.Concurrent;
+
+namespace Nohros.Ruby
+{
+  internal partial class IPCChannel
+  {
+    class ListenerExecutorPair {
+      readonly IRubyMessageListener listener_;
+      readonly IExecutor executor_;
+
+      /// <summary>
+      /// Initializes a new instance of the <see cref="ListenerExecutorPair"/>
+      /// class by using the specified listener and executor.
+      /// </summary>
+      public ListenerExecutorPair(IRubyMessageListener listener, IExecutor executor) {
+        listener_ = listener;
+        executor_ = executor;
+      }
+      
+      /// <summary>
+      /// Gets the <see cref="IExecutor"/> object associated with the pair.
+      /// </summary>
+      public IExecutor Executor {
+        get { return executor_; }
+      }
+
+      /// <summary>
+      /// gets the <see cref="IRubyMessageListener"/> object associated with
+      /// the pair.
+      /// </summary>
+      public IRubyMessageListener Listener {
+        get { return listener_; }
+      }
+    }
+  }
+}
