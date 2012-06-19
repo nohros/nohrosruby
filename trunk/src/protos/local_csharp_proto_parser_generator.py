5c50c4236fe635ebad1534054ef14a5cc2efdfe7
nohros.ceo <nohros.ceo@15e307e2-2acb-11df-b744-258d51847b74>
2012-06-19 18:12:38 +0000

    Added specific charp options to the proto messages.

--- trunk/src/protos/local_csharp_proto_parser_generator.py
+++ trunk/src/protos/local_csharp_proto_parser_generator.py
@@ -8,5 +8,5 @@
 
 import os
 
-os.system('proto_parser_generator.py "protogen.exe --descriptor_set_out=./parsers/csharp/ruby_message_packet.protobin -ignore_google_protobuf=true --include_imports -output_directory=./parsers/csharp/" .')
+os.system('proto_parser_generator.py "protogen.exe --descriptor_set_out=./parsers/csharp/ruby_message_packet.protobin --include_imports -output_directory=./parsers/csharp/" .')
 os.system('pause');
