3b3608d656aa5171b29720f1a483b26aef7a6f4c
nohros.ceo <nohros.ceo@15e307e2-2acb-11df-b744-258d51847b74>
2012-06-25 13:37:06 +0000

    Adapted the script to the python 3.2 runtime.

--- src/protos/proto_parser_generator.py
+++ src/protos/proto_parser_generator.py
@@ -10,7 +10,7 @@ import subprocess
 
 argv_len = len(sys.argv)
 if argv_len < 2:
-  print "Usage protos_compiler.py PROTO_COMPILER_FILE_PATH [proto files to compile] [proto dir]"
+  print("Usage protos_compiler.py PROTO_COMPILER_FILE_PATH [proto files to compile] [proto dir]")
   sys.exit(0)
 try:
   protoc_path = sys.argv[1]
@@ -29,21 +29,21 @@ try:
     # find all *.protos files with filter
     proto_files = [proto_file for proto_file in os.listdir(proto_dir) if proto_file.endswith(".proto")]
 
-  print "Nohros Inc. Protocol Buffer compiler invocation"
-  print ""
-  print "Compiling *.protos using: "
-  print "  Compiler: " + protoc_path
-  print "  Proto(s) path: " + proto_dir
-  print "  Amount: " + str(len(proto_files))
-  print ""
+  print ("Nohros Inc. Protocol Buffer compiler invocation")
+  print ("")
+  print ("Compiling *.protos using: ")
+  print ("  Compiler: " + protoc_path)
+  print ("  Proto(s) path: " + proto_dir)
+  print ("  Amount: " + str(len(proto_files)))
+  print ("")
   
   # compile all the proto specified proto files
   for proto_file in proto_files:
     script = "\"" + protoc_path + "\" " + proto_dir+"\\"+proto_file
-    print "Executing the compiler script"
-    print "  " + script
+    print ("Executing the compiler script")
+    print ("  " + script)
     os.system(script)
     
 except Exception as inst:
-  print inst
+  print (inst)
   sys.exit(1)
