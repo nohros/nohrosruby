5938fdb92b4971792ec817a2494f58e4312a8f2e
nohros.ceo <nohros.ceo@15e307e2-2acb-11df-b744-258d51847b74>
2012-06-27 21:59:19 +0000

    Create a implementation of the IRubyMessageChannel interface that acts as a /dev/null. It should be used when the appropriate channel is not configured.

---
+++ src/services/net/rubynet/ipc/NullMessageChannel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Nohros.Concurrent;
+using Nohros.Ruby.Protocol;
+
+namespace Nohros.Ruby
+{
+  /// <summary>
+  /// A channel implementation that essentially behaves like "/dev/null". All
+  /// calls to Send() will return <c>true</c> although no action is performed.
+  /// Added listeners are ignored and the Open method do nothing.
+  /// </summary>
+  public class NullMessageChannel : IRubyMessageChannel
+  {
+    #region .ctor
+    /// <summary>
+    /// Initializes a new instance of the <see cref="NullMessageChannel"/>
+    /// class.
+    /// </summary>
+    public NullMessageChannel() {
+    }
+    #endregion
+
+    /// <inheritdoc/>
+    public bool Send(IRubyMessage message, string service) {
+      return true;
+    }
+
+    /// <inheritdoc/>
+    public void AddListener(IRubyMessageListener listener, IExecutor executor,
+      string service) {
+    }
+
+    /// <inheritdoc/>
+    public bool Send(RubyMessagePacket packet) {
+      return true;
+    }
+
+    /// <inheritdoc/>
+    public void Open() {
+    }
+  }
+}
