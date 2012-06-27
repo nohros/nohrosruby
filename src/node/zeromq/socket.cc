be4a597180eaf10838a5d6deb24eba482783bce4
nohros.ceo <nohros.ceo@15e307e2-2acb-11df-b744-258d51847b74>
2012-06-27 22:03:27 +0000

    Changed the signature of the Receive function of the Socket class to allow callers to receive multi-part messages.

--- src/node/zeromq/socket.cc
+++ src/node/zeromq/socket.cc
@@ -49,10 +49,24 @@ bool Socket::Send(Message* message, int size, SocketFlags flags) {
     zmq_send(ref_->socket(), message->message(), flags)) == ZMQ_OK;
 }
 
-scoped_refptr<Message> Socket::Receive(SocketFlags flags) {
-  Message* message = new Message();
-  CheckError(zmq_recv(ref_->socket(), message->message(), flags));
-  return message;
+std::vector<scoped_refptr<Message>> Socket::Receive(SocketFlags flags) {
+  bool has_more = true;
+  size_t more_size;
+  std::vector<scoped_refptr<Message>> message_parts;
+  do {
+    // Get the next message part from socket.
+    Message* message = new Message();
+    if (CheckError(
+      zmq_recv(ref_->socket(), message->message(), flags)) != ZMQ_OK) {
+      break;
+    }
+    message_parts.push_back(message);
+
+    // Check if the message is a multipart message.
+    int err = zmq_getsockopt(ref_->socket(), ZMQ_RCVMORE, &has_more, &more_size);
+    has_more = (CheckError(err) != ZMQ_OK) && has_more;
+  } while (has_more);
+  return message_parts;
 }
 
 int Socket::CheckError(int err) {
