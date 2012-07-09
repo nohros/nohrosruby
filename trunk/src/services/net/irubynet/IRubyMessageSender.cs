baac5688c9ed095ead149e1b3751774aa8f363b3
nohros.ceo <nohros.ceo@15e307e2-2acb-11df-b744-258d51847b74>
2012-07-09 22:25:43 +0000

    Moved the services creation logic to the same thread that runs the services. This was done to ensure that the objects that are created at service initialization does not cross the boundaries of the service thread.

--- trunk/src/services/net/irubynet/IRubyMessageSender.cs
+++ trunk/src/services/net/irubynet/IRubyMessageSender.cs
@@ -10,19 +10,26 @@ namespace Nohros.Ruby
   public interface IRubyMessageSender
   {
     /// <summary>
-    /// Sends a ruby message to the service identified by
-    /// <paramref name="service"/>.
+    /// Sends a ruby message to the ruby service node.
     /// </summary>
     /// <param name="message">
     /// The message to send.
-    /// </param>
-    /// <param name="service">
-    /// The name of the service to which the message is destinated.
     /// </param>
     /// <returns>
     /// <c>true</c> is the message was succesfully sent; otherwise,
     /// <c>false</c>.
     /// </returns>
-    bool Send(IRubyMessage message, string service);
+    /// <remarks>
+    /// This method is used to send a message to the service node that is
+    /// hosting a service. It is usually used to send a reply to a messages
+    /// that was successfully received and processed.
+    /// <para>Ruby does not impose a
+    /// strict request/reply message pattern. A received message could have
+    /// or not a reply, the service that is receiving the message and the
+    /// sender should establish a message pattern that is most suitable for its
+    /// operation.
+    /// </para>
+    /// </remarks>
+    bool Send(IRubyMessage message);
   }
 }
