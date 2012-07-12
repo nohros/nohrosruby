8d4f00cd93e456c90e21f599ef086b281be9dfd4
nohros.ceo <nohros.ceo@15e307e2-2acb-11df-b744-258d51847b74>
2012-07-12 20:32:26 +0000

    Created a factory to build instances of the aggregator service.

---
+++ src/services/net/weblog/AggregatorFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using ZMQ;
+
+namespace Nohros.Ruby.Logging
+{
+  public class AggregatorFactory : IRubyServiceFactory
+  {
+    public IRubyService CreateService(string command_line_string) {
+      AppFactory app = new AppFactory();
+      return CreateAggregator(app.LoadSettings());
+    }
+
+    public Aggregator CreateAggregator(IAggregatorSettings settings) {
+      return new Aggregator(new Context(), settings);
+    }
+  }
+}
