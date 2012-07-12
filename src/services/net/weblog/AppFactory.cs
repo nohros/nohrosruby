dfe4fbfe34422f602d9fa538e07e2a59642fdee5
nohros.ceo <nohros.ceo@15e307e2-2acb-11df-b744-258d51847b74>
2012-07-12 20:34:33 +0000

    Removed service specific factory functions from the main app factory.

--- src/services/net/weblog/AppFactory.cs
+++ src/services/net/weblog/AppFactory.cs
@@ -1,53 +1,42 @@
 ﻿using System;
+using System.IO;
+using System.Reflection;
 using Nohros.Logging;
 using Nohros.Configuration;
 using Nohros.Providers;
-using ZMQ;
 
 namespace Nohros.Ruby.Logging
 {
   public sealed class AppFactory
   {
-    readonly Settings settings_;
+    public Settings LoadSettings() {
+      string assembly_location = Assembly.GetExecutingAssembly().Location;
+      string config_file_path =
+        Path.Combine(Path.GetDirectoryName(assembly_location),
+          Strings.kConfigFileName);
 
-    #region .ctor
-    /// <summary>
-    /// Initializes a new instance of the <see cref="AppFactory"/> object using
-    /// the specified <see cref="Settings"/> object.
-    /// </summary>
-    /// <param name="settings">
-    /// A <see cref="Settings"/> objects containing the configured application
-    /// settings.
-    /// </param>
-    public AppFactory(Settings settings) {
-      settings_ = settings;
-    }
-    #endregion
-
-    public Aggregator CreateAggergator() {
-      Context context = new Context();
-      return new Aggregator(
-        GetDealerSocket(context), GetPublisherSocket(context), settings_);
+      Settings settings = new Settings();
+      settings.Load(config_file_path, Strings.kRootFileName);
+      ConfigureLogger(settings);
+      settings.AggregatorDataProvider = GetAggregatorDataProvider(settings);
+      return settings;
     }
 
-    Socket GetDealerSocket(Context context) {
-      Socket socket = context.Socket(SocketType.DEALER);
-      socket.Bind("tcp://*:" + settings_.ListenerPort);
-      return socket;
+    IAggregatorDataProvider GetAggregatorDataProvider(IAggregatorSettings settings) {
+      IProviderNode provider =
+        settings.Providers[Strings.kAggregatorDataProvider];
+      return
+        ProviderFactory<IAggregatorDataProviderFactory>
+          .CreateProviderFactoryFallback(provider, settings)
+          .CreateAggregatorDataProvider(provider.Options);
     }
 
-    Socket GetPublisherSocket(Context context) {
-      Socket socket = context.Socket(SocketType.PUB);
-      socket.Bind("tcp://*:" + settings_.PublisherPort);
-      return socket;
-    }
-
-    public void ConfigureLogger() {
-      IProviderNode provider = settings_.Providers[Strings.kLoggingProviderName];
+    void ConfigureLogger(ISettings settings) {
+      IProviderNode provider = settings.Providers[Strings.kLoggingProviderName];
       ILogger logger = ProviderFactory<ILoggerFactory>
         .CreateProviderFactory(provider)
-        .CreateLogger(provider.Options, settings_);
-      AggregatorLogger.ForCurrentProcess = new AggregatorLogger(logger);
+        .CreateLogger(provider.Options, settings);
+      RubyLogger.ForCurrentProcess = new RubyLogger(logger);
     }
   }
 }
