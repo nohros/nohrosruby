194d91dfab4d819e71ba38231b726024949b0f45
nohros.ceo <nohros.ceo@15e307e2-2acb-11df-b744-258d51847b74>
2012-07-16 22:40:09 +0000

    Defined a new function on the IAggregatorDataProvider interface to allows the processing of the new StorageMessage proto.

--- src/services/net/weblog/properties/AssemblyInfo.cs
+++ src/services/net/weblog/properties/AssemblyInfo.cs
@@ -5,13 +5,13 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("weblog")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Ruby Logging Service.")]
+[assembly: AssemblyDescription("Ruby Logging Service.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("weblog")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCompany("Nohros Inc.")]
+[assembly: AssemblyProduct("Nohros Ruby.")]
+[assembly: AssemblyCopyright("Copyright ©  2012 by Nohros Inc.")]
+[assembly: AssemblyTrademark("Nohros Inc.")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
