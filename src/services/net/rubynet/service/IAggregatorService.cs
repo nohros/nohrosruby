6856cc13017301808cedfbe82850549a5fde3ff6
nohros.ceo <nohros.ceo@15e307e2-2acb-11df-b744-258d51847b74>
2012-07-25 19:22:04 +0000

    Implemented the IRubyServiceHost.Logger property using the ruby logger aggregator service.

---
+++ src/services/net/rubynet/service/IAggregatorService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Nohros.Ruby.Logging;
+
+namespace Nohros.Ruby
+{
+  /// <summary>
+  /// A class that log messages using the ruby log aggregator service.
+  /// </summary>
+  public interface IAggregatorService
+  {
+    /// <summary>
+    /// Send a <see cref="LogMessage"/> to the log aggregator service.
+    /// </summary>
+    /// <param name="log">
+    /// The log message to send.
+    /// </param>
+    void Log(LogMessage log);
+  }
+}
---
+++ src/services/net/rubynet/service/LoggerAggregatorService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Nohros.Data.Json;
+using Nohros.Ruby.Logging;
+
+namespace Nohros.Ruby
+{
+  /// <summary>
+  /// An implementation of the <see cref="IAggregatorService"/> that uses
+  /// the currently configured logger to log messages.
+  /// </summary>
+  public class LoggerAggregatorService : IAggregatorService
+  {
+    readonly IRubyLogger logger_;
+
+    #region .ctor
+    /// <summary>
+    /// Initializes a new instance of the <see cref="LoggerAggregatorService"/>
+    /// </summary>
+    public LoggerAggregatorService() {
+      logger_ = RubyLogger.ForCurrentProcess;
+    }
+    #endregion
+
+    /// <inheritdoc/>
+    public void Log(LogMessage log) {
+      logger_.Info(new JsonStringBuilder()
+        .WriteBeginObject()
+        .WriteMember("application", log.Application)
+        .WriteMember("level", log.Level)
+        .WriteMember("reason", log.Reason)
+        .WriteMember("user", log.User)
+        .ToString());
+    }
+  }
+}
