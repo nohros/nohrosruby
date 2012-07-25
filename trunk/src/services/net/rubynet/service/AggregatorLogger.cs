23675eaa8fb2198f5efa047069e919fc644486b1
nohros.ceo <nohros.ceo@15e307e2-2acb-11df-b744-258d51847b74>
2012-07-25 19:22:04 +0000

    Implemented the IRubyServiceHost.Logger property using the ruby logger aggregator service.

---
+++ trunk/src/services/net/rubynet/service/AggregatorLogger.cs
@@ -0,0 +1,186 @@
+﻿using System;
+
+using Nohros.Logging;
+using Nohros.Ruby.Logging;
+
+namespace Nohros.Ruby
+{
+  internal class AggregatorLogger : IRubyLogger
+  {
+    readonly string application_;
+    IAggregatorService aggregator_service_;
+
+    #region .ctor
+    /// <summary>
+    /// Initializes a new instance of the <see cref="AggregatorLogger"/> class
+    /// using the specified application name, aggregator_service and log level.
+    /// </summary>
+    /// <param name="application">
+    /// The name of the application associated with the logger.
+    /// </param>
+    /// <param name="aggregator_service">
+    /// A <see cref="AggregatorService"/> object that is used to send the log
+    /// message to the log aggregator_service service.
+    /// </param>
+    public AggregatorLogger(string application,
+      IAggregatorService aggregator_service) {
+      aggregator_service_ = aggregator_service;
+      application_ = application;
+    }
+    #endregion
+
+    /// <inheritdoc/>
+    public void Debug(string message) {
+      Log(message, LogLevel.Debug);
+    }
+
+    /// <inheritdoc/>
+    public void Debug(string message, Exception exception) {
+      Log(message, LogLevel.Debug, exception);
+    }
+
+    /// <inheritdoc/>
+    public void Error(string message) {
+      Log(message, LogLevel.Error);
+    }
+
+    /// <inheritdoc/>
+    public void Error(string message, Exception exception) {
+      Log(message, LogLevel.Error, exception);
+    }
+
+    /// <inheritdoc/>
+    public void Fatal(string message) {
+      Log(message, LogLevel.Fatal);
+    }
+
+    /// <inheritdoc/>
+    public void Fatal(string message, Exception exception) {
+      Log(message, LogLevel.Fatal, exception);
+    }
+
+    /// <inheritdoc/>
+    public void Info(string message) {
+      Log(message, LogLevel.Info);
+    }
+
+    /// <inheritdoc/>
+    public void Info(string message, Exception exception) {
+      Log(message, LogLevel.Info, exception);
+    }
+
+    /// <inheritdoc/>
+    public void Warn(string message) {
+      Log(message, LogLevel.Warn);
+    }
+
+    /// <inheritdoc/>
+    public void Warn(string message, Exception exception) {
+      Log(message, LogLevel.Warn, exception);
+    }
+
+    /// <inheritdoc/>
+    public bool IsDebugEnabled {
+      get { return RubyLogger.ForCurrentProcess.IsDebugEnabled; }
+    }
+
+    /// <inheritdoc/>
+    public bool IsErrorEnabled {
+      get { return RubyLogger.ForCurrentProcess.IsErrorEnabled; }
+    }
+
+    /// <inheritdoc/>
+    public bool IsFatalEnabled {
+      get { return RubyLogger.ForCurrentProcess.IsFatalEnabled; }
+    }
+
+    /// <inheritdoc/>
+    public bool IsInfoEnabled {
+      get { return RubyLogger.ForCurrentProcess.IsInfoEnabled; }
+    }
+
+    /// <inheritdoc/>
+    public bool IsWarnEnabled {
+      get { return RubyLogger.ForCurrentProcess.IsWarnEnabled; }
+    }
+
+    /// <inheritdoc/>
+    public bool IsTraceEnabled {
+      get { return RubyLogger.ForCurrentProcess.IsTraceEnabled; }
+    }
+
+    /// <summary>
+    /// Gets or sets the <see cref="IAggregatorService"/> object that is used
+    /// to communicate with the aggregator service.
+    /// </summary>
+    public IAggregatorService AggregatorService {
+      get { return aggregator_service_; }
+      set { aggregator_service_ = value; }
+    }
+
+    /// <inheritdoc/>
+    void Log(string message, LogLevel level) {
+      LogMessage.Builder builder = GetLogMessageBuilder(message,
+        GetLogLevel(level));
+      aggregator_service_.Log(builder.Build());
+    }
+
+    /// <inheritdoc/>
+    void Log(string message, LogLevel level, Exception exception) {
+      LogMessage.Builder builder =
+        GetLogMessageBuilder(message, GetLogLevel(level))
+          .AddCategorization(new KeyValuePair.Builder()
+            .SetKey("exception")
+            .SetValue(exception.Message)
+            .Build())
+          .AddCategorization(new KeyValuePair.Builder()
+            .SetKey("backtrace")
+            .SetValue(exception.StackTrace)
+            .Build());
+      aggregator_service_.Log(builder.Build());
+    }
+
+    string GetLogLevel(LogLevel level) {
+      switch (level) {
+        case LogLevel.Debug:
+          return "debug";
+        case LogLevel.Info:
+          return "info";
+        case LogLevel.All:
+          return "all";
+        case LogLevel.Trace:
+          return "trace";
+        case LogLevel.Fatal:
+          return "fatal";
+        case LogLevel.Off:
+          return "off";
+        case LogLevel.Warn:
+          return "warn";
+        case LogLevel.Error:
+          return "error";
+      }
+      throw new ArgumentOutOfRangeException("level");
+    }
+
+    LogMessage.Builder GetLogMessageBuilder(string message, string level) {
+      return new LogMessage.Builder()
+        .SetApplication(application_)
+        .SetLevel(level)
+        .SetReason(message)
+        .SetTimeStamp(TimeUnitHelper.ToUnixTime(DateTime.Now))
+        .SetUser(Environment.UserName)
+        .AddCategorization(new KeyValuePair.Builder()
+          .SetKey("host")
+          .SetValue(Environment.MachineName)
+          .Build())
+        .AddCategorization(new KeyValuePair.Builder()
+          .SetKey("os")
+          .SetValue(Environment.OSVersion.ToString())
+          .Build())
+        .AddCategorization(new KeyValuePair.Builder()
+          .SetKey("clr-version")
+          .SetValue(Environment.Version.ToString())
+          .Build());
+    }
+  }
+}
---
+++ trunk/src/services/net/rubynet/service/AggregatorService.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Nohros.Ruby.Logging;
+using ZMQ;
+using Nohros.Logging;
+using R = Nohros.Resources;
+
+namespace Nohros.Ruby
+{
+  internal class AggregatorService : IAggregatorService
+  {
+    const string kClassName = "Nohros.Ruby.AggregatorService";
+
+    readonly Context context_;
+    readonly Socket dealer_;
+    readonly string log_aggregator_address_;
+    readonly IRubyLogger logger_;
+
+    #region .ctor
+    /// <summary>
+    /// Initializes a new instance of the <see cref="AggregatorService"/>
+    /// class by using the specified <see cref="ILogger"/> interface.
+    /// </summary>
+    public AggregatorService(Context context, string log_aggregator_address) {
+      context_ = context;
+      dealer_ = context_.Socket(SocketType.DEALER);
+      log_aggregator_address_ = log_aggregator_address;
+      logger_ = RubyLogger.ForCurrentProcess;
+    }
+    #endregion
+
+    /// <summary>
+    /// Configures the logger.
+    /// </summary>
+    /// <remarks>
+    /// This method should be called to make the logger usable.
+    /// </remarks>
+    /// <returns>
+    /// <c>true</c> when the logger was successfully configured; otherwise,
+    /// <c>false</c>.
+    /// </returns>
+    public bool Configure() {
+      try {
+        dealer_.Connect("tcp://" + log_aggregator_address_);
+      } catch (System.Exception exception) {
+        logger_.Error(
+          string.Format(R.StringResources.Log_MethodThrowsException, kClassName,
+            "Configure"), exception);
+        return false;
+      }
+      return true;
+    }
+
+    /// <summary>
+    /// Sends the specified log message to the log aggregator.
+    /// </summary>
+    /// <param name="log">The message to log.</param>
+    public void Log(LogMessage log) {
+      try {
+        dealer_.Send(log.ToByteArray());
+      } catch (System.Exception exception) {
+        logger_.Error(
+          string.Format(R.StringResources.Log_MethodThrowsException, kClassName,
+            "Log"), exception);
+      }
+    }
+  }
+}
