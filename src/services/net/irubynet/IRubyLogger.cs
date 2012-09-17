5cbe9b34fbf948155ce002c0a1499c6643996559
nohros.ceo <nohros.ceo@15e307e2-2acb-11df-b744-258d51847b74>
2012-09-17 02:29:09 +0000

    Added a common logging interface.

---
+++ src/services/net/irubynet/IRubyLogger.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using Nohros.Logging;
+
+namespace Nohros.Ruby
+{
+  /// <summary>
+  /// Defines the methods and properties provided by the ruby logging
+  /// infrastructure.
+  /// </summary>
+  public interface IRubyLogger : ILogger
+  {
+    void Debug(string message, IDictionary<string, string> categorization);
+
+    void Debug(string message, Exception exception,
+      IDictionary<string, string> categorization);
+
+    void Error(string message, IDictionary<string, string> categorization);
+
+    void Error(string message, Exception exception,
+      IDictionary<string, string> categorization);
+
+    void Fatal(string message, IDictionary<string, string> categorization);
+
+    void Fatal(string message, Exception exception,
+      IDictionary<string, string> categorization);
+
+    void Info(string message, IDictionary<string, string> categorization);
+
+    void Info(string message, Exception exception,
+      IDictionary<string, string> categorization);
+
+    void Warn(string message, IDictionary<string, string> categorization);
+
+    void Warn(string message, Exception exception,
+      IDictionary<string, string> categorization);
+  }
+}
