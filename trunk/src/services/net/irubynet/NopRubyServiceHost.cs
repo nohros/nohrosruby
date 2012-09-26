2b3a0a7228a9ce8fa2abfe315af1dd3d4974e0bc
nohros.ceo <nohros.ceo@15e307e2-2acb-11df-b744-258d51847b74>
2012-09-26 21:06:50 +0000

    Created an implementation of the IRubyServiceHost interface that do no operation. This is class intend to be used in integration tests or by services that wants to self-service.

---
+++ trunk/src/services/net/irubynet/NopRubyServiceHost.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Nohros.Ruby.Protocol;
+
+namespace Nohros.Ruby
+{
+  /// <summary>
+  /// A implementtion of the <see cref="IRubyServiceHost"/> that do nothing.
+  /// </summary>
+  public class NopRubyServiceHost : IRubyServiceHost
+  {
+    readonly IRubyLogger logger_;
+
+    #region .ctor
+    /// <summary>
+    /// Initializes a new instance of the <see cref="NopRubyServiceHost"/>.
+    /// </summary>
+    public NopRubyServiceHost() {
+      logger_ = new NopRubyLogger();
+    }
+    #endregion
+
+    public bool Send(IRubyMessage message) {
+      return false;
+    }
+
+    public IRubyLogger Logger {
+      get { return logger_; }
+    }
+  }
+}
