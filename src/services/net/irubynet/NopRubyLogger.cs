420b4924b01c1d783029992a2ed689c14aa2e1f2
nohros.ceo <nohros.ceo@15e307e2-2acb-11df-b744-258d51847b74>
2012-09-26 21:06:50 +0000

    Created an implementation of the IRubyServiceHost interface that do no operation. This is class intend to be used in integration tests or by services that wants to self-service.

---
+++ src/services/net/irubynet/NopRubyLogger.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using Nohros.Logging;
+
+namespace Nohros.Ruby
+{
+  public class NopRubyLogger : NOPLogger, IRubyLogger
+  {
+    public void Debug(string message, IDictionary<string, string> categorization) {
+    }
+
+    public void Debug(string message, Exception exception,
+      IDictionary<string, string> categorization) {
+    }
+
+    public void Error(string message, IDictionary<string, string> categorization) {
+    }
+
+    public void Error(string message, Exception exception,
+      IDictionary<string, string> categorization) {
+    }
+
+    public void Fatal(string message, IDictionary<string, string> categorization) {
+    }
+
+    public void Fatal(string message, Exception exception,
+      IDictionary<string, string> categorization) {
+    }
+
+    public void Info(string message, IDictionary<string, string> categorization) {
+    }
+
+    public void Info(string message, Exception exception,
+      IDictionary<string, string> categorization) {
+    }
+
+    public void Warn(string message, IDictionary<string, string> categorization) {
+    }
+
+    public void Warn(string message, Exception exception,
+      IDictionary<string, string> categorization) {
+    }
+  }
+}
