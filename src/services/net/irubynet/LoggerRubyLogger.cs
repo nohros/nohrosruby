30c22636cc8fabc19aa02d0ec5827536f9ef5cae
nohros.ceo <nohros.ceo@15e307e2-2acb-11df-b744-258d51847b74>
2012-10-16 13:41:41 +0000

    Created implementation of the IRubyLogger and IRubyServiceHost interface that fowards it method calls to a logger.

---
+++ src/services/net/irubynet/LoggerRubyLogger.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Nohros.Logging;
+
+namespace Nohros.Ruby
+{
+  /// <summary>
+  /// A implementation of the <see cref="IRubyLogger"/> that forwards its
+  /// methods to a <see cref="ILogger"/> object.
+  /// </summary>
+  public class LoggerRubyLogger : ForwardingLogger, IRubyLogger
+  {
+    readonly ILogger logger_;
+
+    #region .ctor
+    /// <summary>
+    /// Initializes a new instance of the <see cref="RubyLogger"/>
+    /// class by using the specified <see cref="ILogger"/> interface.
+    /// </summary>
+    public LoggerRubyLogger(ILogger logger)
+      : base(logger) {
+      logger_ = logger;
+    }
+    #endregion
+
+    /// <inheritdoc/>
+    public void Debug(string message, IDictionary<string, string> categorization) {
+      logger_.Debug(GetMessage(message, categorization));
+    }
+
+    /// <inheritdoc/>
+    public void Debug(string message, Exception exception,
+      IDictionary<string, string> categorization) {
+      logger_.Debug(GetMessage(message, categorization), exception);
+    }
+
+    /// <inheritdoc/>
+    public void Error(string message, IDictionary<string, string> categorization) {
+      logger_.Error(GetMessage(message, categorization));
+    }
+
+    /// <inheritdoc/>
+    public void Error(string message, Exception exception,
+      IDictionary<string, string> categorization) {
+      logger_.Error(GetMessage(message, categorization), exception);
+    }
+
+    /// <inheritdoc/>
+    public void Fatal(string message, IDictionary<string, string> categorization) {
+      logger_.Fatal(GetMessage(message, categorization));
+    }
+
+    /// <inheritdoc/>
+    public void Fatal(string message, Exception exception,
+      IDictionary<string, string> categorization) {
+      logger_.Fatal(GetMessage(message, categorization), exception);
+    }
+
+    /// <inheritdoc/>
+    public void Info(string message, IDictionary<string, string> categorization) {
+      logger_.Info(GetMessage(message, categorization));
+    }
+
+    /// <inheritdoc/>
+    public void Info(string message, Exception exception,
+      IDictionary<string, string> categorization) {
+      logger_.Info(GetMessage(message, categorization), exception);
+    }
+
+    /// <inheritdoc/>
+    public void Warn(string message, IDictionary<string, string> categorization) {
+      logger_.Warn(GetMessage(message, categorization));
+    }
+
+    /// <inheritdoc/>
+    public void Warn(string message, Exception exception,
+      IDictionary<string, string> categorization) {
+      logger_.Warn(GetMessage(message, categorization), exception);
+    }
+
+    string GetMessage(string message, IDictionary<string, string> categorization) {
+      if (categorization.Count > 0) {
+        StringBuilder builder = new StringBuilder(message);
+        builder.Append(" Categorization =>");
+        foreach (KeyValuePair<string, string> pair in categorization) {
+          builder
+            .Append(pair.Key)
+            .Append(":")
+            .Append(pair.Value);
+        }
+        return builder.ToString();
+      }
+      return message;
+    }
+  }
+}
---
+++ src/services/net/irubynet/LoggerRubyServiceHost.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using Nohros.Ruby.Protocol;
+
+namespace Nohros.Ruby
+{
+  /// <summary>
+  /// A implementtion of the <see cref="IRubyServiceHost"/> that send messages
+  /// to a <see cref="IRubyLogger"/>.
+  /// </summary>
+  public class LoggerRubyServiceHost : IRubyServiceHost
+  {
+    readonly IRubyLogger logger_;
+
+    #region .ctor
+    /// <summary>
+    /// Initializes a new instance of the <see cref="NopRubyServiceHost"/>.
+    /// </summary>
+    public LoggerRubyServiceHost(IRubyLogger logger) {
+      logger_ = logger;
+    }
+    #endregion
+
+    public bool Send(IRubyMessage message) {
+      throw new NotImplementedException();
+    }
+
+    public bool Send(int id, int type, byte[] message) {
+      throw new NotImplementedException();
+    }
+
+    public IRubyLogger Logger {
+      get { return logger_; }
+    }
+
+    public bool SendError(int message_id, string error, int exception_code) {
+      logger_.Error(exception_code + " " + error, new Dictionary<string, string>
+      {
+        {"messageId", message_id.ToString()},
+      });
+      return true;
+    }
+
+    public bool SendError(int message_id, string error, int exception_code,
+      Exception exception) {
+      logger_.Error(exception_code + " " + error, exception,
+        new Dictionary<string, string>
+        {
+          {"messageId", message_id.ToString()},
+        });
+      return true;
+    }
+
+    public bool SendError(int message_id, int exception_code,
+      Exception exception) {
+      logger_.Error(exception_code.ToString(), exception,
+        new Dictionary<string, string>
+        {
+          {"messageId", message_id.ToString()},
+        });
+      return true;
+    }
+  }
+}
