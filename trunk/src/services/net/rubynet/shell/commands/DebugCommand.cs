d18100630b324c5360eec8e3d81f52840989cdae
nohros.ceo <nohros.ceo@15e307e2-2acb-11df-b744-258d51847b74>
2012-10-25 12:05:45 +0000

    Created a new command that allow the debugger to be attached to a running service.

---
+++ trunk/src/services/net/rubynet/shell/commands/DebugCommand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics;
+using Nohros.Ruby.Shell;
+
+namespace Nohros.Ruby
+{
+  /// <summary>
+  /// Launches and attaches a debugger to the process.
+  /// </summary>
+  internal class DebugCommand : ShellCommand
+  {
+    #region .ctor
+    /// <summary>
+    /// Initializes a new instance of the <see cref="DebugCommand"/>.
+    /// </summary>
+    public DebugCommand() : base(ShellStrings.kDebugCommand) {
+    }
+    #endregion
+
+    /// <summary>
+    /// Launches and attaches a debugger to the process.
+    /// </summary>
+    /// <param name="process">
+    /// The <see cref="ShellRubyProcess"/> that is running the command.
+    /// </param>
+    public override void Run(ShellRubyProcess process) {
+      Debugger.Launch();
+    }
+  }
+}
