19462152c57fcab25df4f4c06b9b80697c7a4099
nohros.ceo <nohros.ceo@15e307e2-2acb-11df-b744-258d51847b74>
2013-01-21 20:19:23 +0000

    Created a project to make easy the comunication between a aspnet application and a ruby service.

---
+++ trunk/src/services/net/aspnet/QueryResponse.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Google.ProtocolBuffers;
+using Nohros.Concurrent;
+
+namespace Nohros.Ruby
+{
+  public struct QueryRequestFuture
+  {
+    readonly AsyncCallback callback_;
+    readonly SettableFuture<byte[]> response_;
+    readonly object state_;
+
+    #region .ctor
+    public QueryRequestFuture(SettableFuture<byte[]> response,
+      AsyncCallback callback,
+      object state) {
+      response_ = response;
+      callback_ = callback;
+      state_ = state;
+    }
+    #endregion
+
+    public SettableFuture<byte[]> Response {
+      get { return response_; }
+    }
+
+    public AsyncCallback Callback {
+      get { return callback_; }
+    }
+  }
+}
