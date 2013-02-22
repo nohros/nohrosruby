05f347f082f0fa0fb27999fe166d0847ab2701b1
nohros.ceo <nohros.ceo@15e307e2-2acb-11df-b744-258d51847b74>
2013-02-22 19:10:32 +0000

    Fix a bug that causes the RubyMessageChannel to block when a message is sent.

---
+++ trunk/src/services/net/rubynet/ipc/NullMessageSender.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using Nohros.Ruby.Protocol;
+
+namespace Nohros.Ruby
+{
+  /// <summary>
+  /// A channel implementation that essentially behaves like "/dev/null". All
+  /// calls to Send() will return <c>true</c> although no action is performed.
+  /// Added listeners are ignored.
+  /// </summary>
+  public class NullMessageSender : IRubyMessageSender, IRubyMessageSenderFactory
+  {
+    public bool Send(IRubyMessage message) {
+      return true;
+    }
+
+    public bool Send(IRubyMessage message, IEnumerable<KeyValuePair<string, string>> facts) {
+      return false;
+    }
+
+    public bool Send(byte[] message_id, int type, byte[] message, byte[] destination, IEnumerable<KeyValuePair<string, string>> facts) {
+      return false;
+    }
+
+    public bool Send(byte[] message_id, int type, byte[] message, byte[] destination, string token, IEnumerable<KeyValuePair<string, string>> facts) {
+      return false;
+    }
+
+    public bool Send(byte[] message_id, int type, byte[] message,
+      byte[] destination) {
+      return true;
+    }
+
+    public bool Send(byte[] message_id, int type, byte[] message,
+      byte[] destination, string token) {
+      return true;
+    }
+
+    IRubyMessageSender IRubyMessageSenderFactory.CreateRubyMessageSender() {
+      return new NullMessageSender();
+    }
+  }
+}
