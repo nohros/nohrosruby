81f51b03e5575532697818fa86b78b8a26173816
nohros.ceo <nohros.ceo@15e307e2-2acb-11df-b744-258d51847b74>
2013-03-07 19:44:52 +0000

    Added methods to the IRubyMessageHost interface that facilitates the process of sending a message.

--- trunk/src/services/net/irubynet/IRubyMessageSender.cs
+++ trunk/src/services/net/irubynet/IRubyMessageSender.cs
@@ -34,7 +34,7 @@ namespace Nohros.Ruby
     bool Send(IRubyMessage message);
 
     /// <summary>
-    /// Sends a ruby message to the ruby service node.
+    /// Sends a ruby message to the ruby service node located at.
     /// </summary>
     /// <param name="message">
     /// The message to send.
@@ -57,7 +57,40 @@ namespace Nohros.Ruby
     /// operation.
     /// </para>
     /// </remarks>
-    bool Send(IRubyMessage message, IEnumerable<KeyValuePair<string,string>> facts);
+    bool Send(IRubyMessage message,
+      IEnumerable<KeyValuePair<string, string>> facts);
+
+    /// <summary>
+    /// Sends a ruby message to the ruby service.
+    /// </summary>
+    /// <param name="message">
+    /// The encoded message to be sent.
+    /// </param>
+    /// <param name="type">
+    /// A integer that identifies the type of the message.
+    /// </param>
+    /// <param name="message_id">
+    /// The ID associated with the message.
+    /// </param>
+    /// <returns>
+    /// <c>true</c> is the message was succesfully sent; otherwise,
+    /// <c>false</c>.
+    /// </returns>
+    /// <remarks>
+    /// This method is used to send a message to the service node that is
+    /// hosting a service. It is usually used to send a reply to a messages
+    /// that was successfully received and processed.
+    /// <para>
+    /// Ruby does not impose a strict request/reply message pattern. A received
+    /// message could have or not a reply, the service that is receiving the
+    /// message and the sender should establish a message pattern that is most
+    /// suitable for its operation.
+    /// </para>
+    /// <para>
+    /// When sending a reply the destination address should be
+    /// </para>
+    /// </remarks>
+    bool Send(byte[] message_id, int type, byte[] message);
 
     /// <summary>
     /// Sends a ruby message to the ruby service.
@@ -94,6 +127,42 @@ namespace Nohros.Ruby
     /// </para>
     /// </remarks>
     bool Send(byte[] message_id, int type, byte[] message, byte[] destination);
+
+    /// <summary>
+    /// Sends a ruby message to the ruby service.
+    /// </summary>
+    /// <param name="message">
+    /// The encoded message to be sent.
+    /// </param>
+    /// <param name="type">
+    /// A integer that identifies the type of the message.
+    /// </param>
+    /// <param name="message_id">
+    /// The ID associated with the message.
+    /// </param>
+    /// <param name="facts">
+    /// A collection of key value pairs containing the message facts.
+    /// </param>
+    /// <returns>
+    /// <c>true</c> is the message was succesfully sent; otherwise,
+    /// <c>false</c>.
+    /// </returns>
+    /// <remarks>
+    /// This method is used to send a message to the service node that is
+    /// hosting a service. It is usually used to send a reply to a messages
+    /// that was successfully received and processed.
+    /// <para>
+    /// Ruby does not impose a strict request/reply message pattern. A received
+    /// message could have or not a reply, the service that is receiving the
+    /// message and the sender should establish a message pattern that is most
+    /// suitable for its operation.
+    /// </para>
+    /// <para>
+    /// When sending a reply the destination address should be
+    /// </para>
+    /// </remarks>
+    bool Send(byte[] message_id, int type, byte[] message,
+      IEnumerable<KeyValuePair<string, string>> facts);
 
     /// <summary>
     /// Sends a ruby message to the ruby service.
@@ -144,7 +213,39 @@ namespace Nohros.Ruby
     /// <param name="type">
     /// A integer that identifies the type of the message.
     /// </param>
-    /// <param name="id">
+    /// <param name="message_id">
+    /// The ID associated with the message.
+    /// </param>
+    /// <returns>
+    /// <c>true</c> is the message was succesfully sent; otherwise,
+    /// <c>false</c>.
+    /// </returns>
+    /// <param name="token">
+    /// A string that identifies the message type.
+    /// </param>
+    /// <remarks>
+    /// This method is used to send a message to the service node that is
+    /// hosting a service. It is usually used to send a reply to a messages
+    /// that was successfully received and processed.
+    /// <para>
+    /// Ruby does not impose a strict request/reply message pattern. A received
+    /// message could have or not a reply, the service that is receiving the
+    /// message and the sender should establish a message pattern that is most
+    /// suitable for its operation.
+    /// </para>
+    /// </remarks>
+    bool Send(byte[] message_id, int type, byte[] message, string token);
+
+    /// <summary>
+    /// Sends a ruby message to the ruby service.
+    /// </summary>
+    /// <param name="message">
+    /// The encoded message to be sent.
+    /// </param>
+    /// <param name="type">
+    /// A integer that identifies the type of the message.
+    /// </param>
+    /// <param name="message_id">
     /// The ID associated with the message.
     /// </param>
     /// <returns>
@@ -169,8 +270,8 @@ namespace Nohros.Ruby
     /// suitable for its operation.
     /// </para>
     /// </remarks>
-    bool Send(byte[] message_id, int type, byte[] message, byte[] destination,
-      string token);
+    bool Send(byte[] message_id, int type, byte[] message, string token,
+      byte[] destination);
 
     /// <summary>
     /// Sends a ruby message to the ruby service.
@@ -181,7 +282,43 @@ namespace Nohros.Ruby
     /// <param name="type">
     /// A integer that identifies the type of the message.
     /// </param>
-    /// <param name="id">
+    /// <param name="message_id">
+    /// The ID associated with the message.
+    /// </param>
+    /// <returns>
+    /// <c>true</c> is the message was succesfully sent; otherwise,
+    /// <c>false</c>.
+    /// </returns>
+    /// <param name="token">
+    /// A string that identifies the message type.
+    /// </param>
+    /// <param name="facts">
+    /// A collection of key value pairs containing the message facts.
+    /// </param>
+    /// <remarks>
+    /// This method is used to send a message to the service node that is
+    /// hosting a service. It is usually used to send a reply to a messages
+    /// that was successfully received and processed.
+    /// <para>
+    /// Ruby does not impose a strict request/reply message pattern. A received
+    /// message could have or not a reply, the service that is receiving the
+    /// message and the sender should establish a message pattern that is most
+    /// suitable for its operation.
+    /// </para>
+    /// </remarks>
+    bool Send(byte[] message_id, int type, byte[] message, string token,
+      IEnumerable<KeyValuePair<string, string>> facts);
+
+    /// <summary>
+    /// Sends a ruby message to the ruby service.
+    /// </summary>
+    /// <param name="message">
+    /// The encoded message to be sent.
+    /// </param>
+    /// <param name="type">
+    /// A integer that identifies the type of the message.
+    /// </param>
+    /// <param name="message_id">
     /// The ID associated with the message.
     /// </param>
     /// <returns>
@@ -209,7 +346,7 @@ namespace Nohros.Ruby
     /// suitable for its operation.
     /// </para>
     /// </remarks>
-    bool Send(byte[] message_id, int type, byte[] message, byte[] destination,
-      string token, IEnumerable<KeyValuePair<string, string>> facts);
+    bool Send(byte[] message_id, int type, byte[] message, string token,
+      byte[] destination, IEnumerable<KeyValuePair<string, string>> facts);
   }
 }
