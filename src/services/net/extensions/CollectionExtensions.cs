677c5530329312ce711b7ef9e6e9de0d16bd513a
nohros.ceo <nohros.ceo@15e307e2-2acb-11df-b744-258d51847b74>
2013-03-21 19:27:10 +0000

    Added extensions methods for collection of classes related with the protocol messages.

---
+++ src/services/net/extensions/CollectionExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Nohros.Ruby
+{
+  public static class CollectionExtensions
+  {
+    /// <summary>
+    /// Determines whether the <paramref name="collection"/> contains a
+    /// specific value.
+    /// </summary>
+    /// <param name="collection">
+    /// An aray of strings to serach for <paramref name="value"/>.
+    /// </param>
+    /// <param name="value">
+    /// The string to locate in the <paramref name="collection"/>
+    /// </param>
+    /// <returns>
+    /// <c>true</c> if <paramref name="value"/> is found in the
+    /// <paramref name="collection"/>; otherwise, <c>false</c>.
+    /// </returns>
+    public static bool Contains(this string[] collection, string value,
+      StringComparison comparison_type) {
+      foreach (var s in collection) {
+        if (string.Compare(s, value, comparison_type) == 0) {
+          return true;
+        }
+      }
+      return false;
+    }
+  }
+}
