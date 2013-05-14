f0ef5c7447f403208b942c1d54c033d6f51e6f6a
nohros.ceo <nohros.ceo@15e307e2-2acb-11df-b744-258d51847b74>
2013-05-14 23:43:09 +0000

    Moved the message handling logic to specific classes.

---
+++ trunk/src/services/net/tracker/QueryMessageHandler.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Nohros.Ruby.Protocol;
+using Nohros.Ruby.Protocol.Control;
+
+namespace Nohros.Ruby
+{
+  internal class QueryMessageHandler : IMessageHandler
+  {
+    readonly IRubyMessageChannel ruby_message_channel_;
+    readonly TrackerEngine trackers_;
+
+    #region .ctor
+    /// <summary>
+    /// Initializes a new instance of the <see cref="QueryMessageHandler"/>
+    /// class by using the specified tracker engine and message sender.
+    /// </summary>
+    /// <param name="trackers">
+    /// A <see cref="TrackerEngine"/> that can be used to process the query.
+    /// </param>
+    /// <param name="ruby_message_channel">
+    /// A <see cref="IRubyMessageChannel"/> that can be used to send the result
+    /// of the query message processing.
+    /// </param>
+    public QueryMessageHandler(TrackerEngine trackers,
+      IRubyMessageChannel ruby_message_channel) {
+      trackers_ = trackers;
+      ruby_message_channel_ = ruby_message_channel;
+    }
+    #endregion
+
+    /// <inheritdoc/>
+    public void Handle(IRubyMessage message) {
+      QueryMessage query = QueryMessage.ParseFrom(message.Message);
+      trackers_.FindServices(KeyValuePairs.ToKeyValuePairs(query.FactsList),
+        endpoint => {
+          var response = CreateMessagePacket(message.Id, endpoint,
+            message.Sender);
+          ruby_message_channel_.Send(response);
+        });
+    }
+
+    RubyMessagePacket CreateMessagePacket(byte[] request_id,
+      ZMQEndPoint endpoint, byte[] destination) {
+      ResponseMessage response = new ResponseMessage.Builder()
+        .AddAddresses(endpoint.Endpoint)
+        .Build();
+      return RubyMessages.CreateMessagePacket(request_id,
+        (int) NodeMessageType.kNodeResponse, response.ToByteArray(), destination);
+    }
+  }
+}
