1a09e3a0fc31ad688110b708d1ef8dea92ee5132
nohros.ceo <nohros.ceo@15e307e2-2acb-11df-b744-258d51847b74>
2013-05-14 23:37:54 +0000

    Added the nohros.data.mongodb library.

---
+++ trunk/src/services/net/rubylog/service/data/mongodb/MongoDataProviderFactory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using MongoDB.Driver;
+using Nohros.Data.MongoDB;
+
+namespace Nohros.Ruby.Logging.Data.MongoDB
+{
+  public class MongoDataProviderFactory : ILogMessageRepositoryFactory
+  {
+    public ILogMessageRepository CreateAggregatorDataProvider(
+      IDictionary<string, string> options) {
+      var factory = new MongoDatabaseProviderFactory();
+      MongoDatabaseProvider provider = factory.CreateProvider(options);
+      MongoDatabase database = provider.GetDatabase();
+      return new MongoDataProvider(database);
+    }
+  }
+}
