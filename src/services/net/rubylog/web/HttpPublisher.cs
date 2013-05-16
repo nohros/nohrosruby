8d7e496cd1363cfc860cb1751381f4d2f800f485
nohros.ceo <nohros.ceo@15e307e2-2acb-11df-b744-258d51847b74>
2013-05-16 19:27:25 +0000

    Change the Properties case

---
+++ src/services/net/rubylog/web/HttpPublisher.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.AspNet.SignalR;
+
+namespace Nohros.Ruby.Logging
+{
+  /// <summary>
+  /// Publish log message over the HTTP channel.
+  /// </summary>
+  /// <remarks>
+  /// The messages are serialized using the JSON format.
+  /// </remarks>
+  public class HttpPublisher : PersistentConnection
+  {
+  }
+}
