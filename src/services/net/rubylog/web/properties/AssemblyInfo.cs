0b28b231d8a3c2ef6652a550f62eaff285b2057d
nohros.ceo <nohros.ceo@15e307e2-2acb-11df-b744-258d51847b74>
2013-05-16 21:47:22 +0000

    Create the channel that will handle the communication between the HTTP server and logging server

--- src/services/net/rubylog/web/properties/AssemblyInfo.cs
+++ src/services/net/rubylog/web/properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("web")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("nohros.ruby.logging.web")]
+[assembly: AssemblyDescription("nohros.ruby.logging.web")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("web")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCompany("Nohros Inc")]
+[assembly: AssemblyProduct("Nohros Ruby.")]
+[assembly: AssemblyCopyright("Copyright ©  2013 by Nohros Inc.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
